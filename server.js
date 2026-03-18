37a56be482f915f328e3e439566c1b0e5c736c12
felipe ignacio <thebigshow@Mac-mini-de-felipe.local>
2026-03-18 00:05:15 -0300

    fix encoding utf8

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use((req, res, next) => { res.setHeader('Content-Type', 'application/json; charset=utf-8'); next(); })
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
